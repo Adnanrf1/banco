1b15cc2cfe32cfcf9f4c6354b0e943de34500123
Adnanrf1 <50079274+Adnanrf1@users.noreply.github.com>
2019-10-20 18:06:03 -0300

    Primeiro commit da atividade

---
+++ Fila.py
@@ -0,0 +1,44 @@
+####################################################################
+# abaixo segue o codigo responsavel por gerar a fila sem prioridade#
+####################################################################
+class Fila:
+    def __init__(self):
+        self.fila = []
+
+    # def que insere a senha em uma lista sem prioridade
+    def inserir(self,n):
+        self.fila.append(n)
+    # def que tira  a senha em uma lista sem prioridade
+    def excluir(self):
+        if not self.vazia():
+            return self.fila.pop(0)
+    # def que busca o tamanho da lista sem prioridade
+    def tamanho(self):
+        return len(self.fila)
+    # def que verifica se esta vazia a lista sem prioridade
+    def vazia(self):
+        return self.tamanho() == 0
+
+
+
+
+
+class FilaPrdd:
+    def __init__(self):
+        self.filaprdd = []
+    # def que insere a senha em uma lista com prioridade
+    def inserir(self,n):
+        self.filaprdd.append(n)
+    # def que tira  a senha em uma lista com prioridade
+    def excluir(self):
+        if not self.vazia():
+            return self.filaprdd.pop(0)
+
+    # def que busca o tamanho da lista com prioridade
+    def tamanho(self):
+        return len(self.filaprdd)
+
+    # def que verifica se esta vazia a lista com prioridade
+    def vazia(self):
+        return self.tamanho() == 0
+
